d46ee4f9e02764858978f43711661dcfdae8ced6
cognivore <66186054+cognivore@users.noreply.github.com>
2023-04-25 17:17:25 +0100

    But just wrapping is also an option

--- lib/utc_date_time.ex
+++ lib/utc_date_time.ex
@@ -1,38 +1,9 @@
 defmodule UtcDateTime do
-  @enforce_keys [:year, :month, :day, :hour, :minute, :second] ++
-                  [:time_zone, :zone_abbr, :utc_offset, :std_offset]
+  defstruct [:dt]
 
-  defstruct [
-    :year,
-    :month,
-    :day,
-    :hour,
-    :minute,
-    :second,
-    :time_zone,
-    :zone_abbr,
-    :utc_offset,
-    :std_offset,
-    microsecond: {0, 0},
-    calendar: Calendar.ISO
-  ]
-
-  @type t :: %__MODULE__{
-          year: Calendar.year(),
-          month: Calendar.month(),
-          day: Calendar.day(),
-          calendar: Calendar.calendar(),
-          hour: Calendar.hour(),
-          minute: Calendar.minute(),
-          second: Calendar.second(),
-          microsecond: Calendar.microsecond(),
-          time_zone: Calendar.time_zone(),
-          zone_abbr: Calendar.zone_abbr(),
-          utc_offset: Calendar.utc_offset(),
-          std_offset: Calendar.std_offset()
-        }
+  @type t :: %__MODULE__{dt: DateTime.t()}
 
   def new() do
-    %{DateTime.utc_now() | __struct__: __MODULE__}
+    %__MODULE__{dt: DateTime.utc_now()}
   end
 end
