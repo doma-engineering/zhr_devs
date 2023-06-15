876023f3bf167e4dcdf458f3f116c27b1b85da31
Olexandr Maslo <18098273+maslowalex@users.noreply.github.com>
2023-06-15 12:16:03 +0300

    Problem: submission page is missing (#13)
    Solution:
    - Introduce a router for navigation between pages
    - Implement Submission front-end page with dummy placeholders

---
+++ assets/src/components/submission/Invites.tsx
@@ -0,0 +1,30 @@
+function Invites(props: {invited: string[], interested: string[], testCompleted: boolean}) {
+
+  let application: JSX.Element
+
+  if (props.testCompleted) {
+    application = <button type="submit" className="rounded bg-purple-300 text-white">Apply</button>
+  } else {
+    application = <p className="text-slate-400">You will be able to apply when completing the test</p>
+  }
+
+  return (
+    <div className="px-8 bg-slate-200 rounded leading-loose">
+      <p className="font-semibold">You were invited by</p>
+      {props.invited.map(invitedBy => {
+        return <p>{invitedBy}</p>
+      })}
+
+      <p className="mt-4 font-semibold">Other companies interested</p>
+      {props.interested.map(interested => {
+        return <p>{interested}</p>
+      })}
+
+      <div className="text-center mt-4">
+        {application}
+      </div>
+    </div>
+  );
+}
+
+export default Invites;
--- assets/src/components/tasks/Task.tsx
+++ assets/src/components/tasks/Task.tsx
@@ -1,15 +1,37 @@
-function Task(props: {technology: string, counter: number}) {
+import { Link } from 'react-router-dom'
+
+function Task(props: {technology: string, counter: number, renderLink: boolean}) {
   let backgroundClass: string;
+  let widthClass: string;
+
+  const taskInfo = <>
+    <p className="text-xl capitalize">{props.technology}</p>
+      
+    <p className="mt-6">Attempts: {props.counter} / 2</p>
+  </>
 
   if (props.counter === 2) {
     backgroundClass = "bg-purple-300"
   } else { backgroundClass = "bg-purple-600" }
 
-  return (
-    <div className={"text-white w-2/5 rounded p-4 " + backgroundClass}>
-      <p className="text-xl capitalize">{props.technology}</p>
-      
-      <p className="mt-6">Attempts: {props.counter} / 2</p>
+  if (props.renderLink) { widthClass = 'w-2/5' } else {
+    widthClass = 'w-full'
+  }
+
+  let innerContent: JSX.Element
+
+  if (props.counter < 2 && props.renderLink) {
+    innerContent =
+    <Link to={`submissions/${props.technology}`}>
+      {taskInfo}
+    </Link>
+  } else {
+    innerContent = taskInfo
+  }
+
+  return (    
+    <div className={`text-white rounded ${widthClass} p-4 ${backgroundClass}`}>
+      {innerContent}
     </div>
   )
 }
