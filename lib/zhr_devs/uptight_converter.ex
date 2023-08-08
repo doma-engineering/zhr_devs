204b7c7be06d29ac8c052a1c6c71e70fcda57e43
cognivore <66186054+cognivore@users.noreply.github.com>
2023-08-08 17:47:36 +0100

    Credo

--- lib/zhr_devs/uptight_converter.ex
+++ lib/zhr_devs/uptight_converter.ex
@@ -4,7 +4,12 @@ defmodule ZhrDevs.UptightConverter do
   """
 
   defmodule Sixteen do
-    @spec from(binary | %Uptight.Binary{} | %Uptight.Base.Sixteen{}) :: %Uptight.Base.Sixteen{}
+    @moduledoc """
+    Convenience functions for base 16.
+    """
+
+    @spec from(binary() | Uptight.Binary.t() | Uptight.Base.Sixteen.t()) ::
+            Uptight.Base.Sixteen.t()
     def from(<<x::binary>>) do
       %Uptight.Base.Sixteen{
         encoded: Base.encode16(x, case: :lower),
@@ -20,16 +25,16 @@ defmodule ZhrDevs.UptightConverter do
       |> from()
     end
 
-    @spec from_raw(binary) :: %Uptight.Base.Sixteen{}
+    @spec from_raw(binary) :: Uptight.Base.Sixteen.t()
     def from_raw(<<x::binary>>), do: from(<<x::binary>>)
 
-    @spec from_binary(%Uptight.Binary{}) :: %Uptight.Base.Sixteen{}
+    @spec from_binary(Uptight.Binary.t()) :: Uptight.Base.Sixteen.t()
     def from_binary(%Uptight.Binary{} = x), do: from(x)
 
-    @spec to_binary(%Uptight.Base.Sixteen{}) :: %Uptight.Binary{}
+    @spec to_binary(Uptight.Base.Sixteen.t()) :: Uptight.Binary.t()
     def to_binary(%Uptight.Base.Sixteen{} = x), do: %Uptight.Binary{binary: x.raw}
 
-    @spec to_raw(%Uptight.Base.Sixteen{}) :: binary()
+    @spec to_raw(Uptight.Base.Sixteen.t()) :: binary()
     def to_raw(%Uptight.Base.Sixteen{} = x), do: x.raw
   end
 end
