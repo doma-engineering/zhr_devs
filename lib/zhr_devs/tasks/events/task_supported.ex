1f17992be842e6ad2a4a2b5b8c723bfd4fa653c8
cognivore <66186054+cognivore@users.noreply.github.com>
2023-09-16 16:54:18 +0100

    Problem: Typespec of events does not reflect raw ones.
    Solution:
    
     - Fix the typespec on the example of task_supported.ex, add a typedoc to clarify the idea.
     - Tidy up excessive from_ok.
     - Ensure that decode is idempotent [decode(decode(x)) = decode(x)].

--- lib/zhr_devs/tasks/events/task_supported.ex
+++ lib/zhr_devs/tasks/events/task_supported.ex
@@ -13,11 +13,15 @@ defmodule ZhrDevs.Tasks.Events.TaskSupported do
   @derive Jason.Encoder
   defstruct @fields
 
+  @typedoc """
+  Left side of the `|` (binary()) represent raw values coming from the external world,
+  right side of the `|` (atom() & Text.t()) represent values that are used internally.
+  """
   @type t() :: %{
           :__struct__ => __MODULE__,
-          required(:name) => Urlsafe.t(),
-          required(:task_uuid) => Text.t(),
-          required(:technology) => atom()
+          required(:name) => binary() | atom(),
+          required(:task_uuid) => binary() | Text.t(),
+          required(:technology) => binary() | atom()
         }
 
   def fields, do: @fields
@@ -26,11 +30,17 @@ end
 defimpl Commanded.Serialization.JsonDecoder, for: ZhrDevs.Tasks.Events.TaskSupported do
   alias ZhrDevs.Tasks.Events.TaskSupported
 
-  def decode(%TaskSupported{name: name, task_uuid: task_uuid, technology: technology}) do
+  def decode(%TaskSupported{
+        name: name = <<_::binary>>,
+        task_uuid: task_uuid = <<_::binary>>,
+        technology: technology = <<_::binary>>
+      }) do
     %TaskSupported{
       name: String.to_existing_atom(name),
       task_uuid: Uptight.Text.new!(task_uuid),
       technology: String.to_existing_atom(technology)
     }
   end
+
+  def decode(%TaskSupported{} = event), do: event
 end
