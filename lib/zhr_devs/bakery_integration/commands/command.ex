347eee1b01659a426ce6e9e86a0ad064c53be615
Olexandr <maslo.rails@gmail.com>
2023-11-08 08:40:20 +0000

    fixed tests, refactored code

--- lib/zhr_devs/bakery_integration/commands/command.ex
+++ lib/zhr_devs/bakery_integration/commands/command.ex
@@ -2,14 +2,15 @@ defmodule ZhrDevs.BakeryIntegration.Commands.Command do
   @moduledoc """
   Behaviour for defining commands that can be handled correctly by the CommandRunner
   """
-  @type error() :: %{
+  @type system_error() :: %{
           error: atom(),
           exit_status: nil | integer(),
           context: term(),
           message: term()
         }
 
-  @callback run(opts :: Keyword.t()) :: {:ok, pid()} | {:error, term()} | Uptight.Result.Err.t()
+  @callback run(opts :: Keyword.t()) :: {:ok, pid()} | {:error, term()}
+  @callback build(opts :: Keyword.t()) :: Uptight.Result.t()
   @callback on_success(term()) :: :ok | {:error, term()}
-  @callback on_failure(error()) :: :ok
+  @callback on_failure(system_error()) :: :ok
 end
