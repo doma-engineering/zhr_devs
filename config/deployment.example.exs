2df97d795e65a55f9e2a44a0edc0167b08cbdd45
cognivore <66186054+cognivore@users.noreply.github.com>
2024-01-20 23:56:40 +0000

    Problem: Unclear what to put into deployment.exs
    Solution:
    
     - Add a deployment.example.exs in line with secret example configs

---
+++ config/deployment.example.exs
@@ -0,0 +1,12 @@
+import Config
+
+config :zhr_devs,
+  server_code_folders: %{
+    {:on_the_map, :goo} => "/home/sweater/github/zerohr/tasks/on_the_map/goo"
+  }
+
+config :zhr_devs,
+  submission_uploads_folder: "/tmp/submissions",
+  output_json_backup_folder: "/tmp/output_backup",
+  harvested_tasks_structure: ["priv", "tasks", "harvested"],
+  command_logs_folder: "/tmp/command_logs"
